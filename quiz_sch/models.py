9adff842ea2277d2e444a9ea3ac57fd5ac6e5909
navalkishor007 <67671688+navalkishor007@users.noreply.github.com>
2021-01-05 23:18:56 +0530

    created model and added static files

--- quiz_sch/models.py
+++ quiz_sch/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Question(models.Model):
+    question = models.CharField(max_length=200)
+    option_1 = models.CharField(max_length=20)
+    option_2 = models.CharField(max_length=20)
+    option_3 = models.CharField(max_length=20)
+    option_4 = models.CharField(max_length=20)
+    right_ans = models.CharField(max_length=20)
