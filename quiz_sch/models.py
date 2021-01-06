c4a44659d161725485ea777b8fb5f9d2b9961fc1
navalkishor007 <67671688+navalkishor007@users.noreply.github.com>
2021-01-06 17:23:44 +0530

    Added model and designed exam pattern with base class functinality working on exam logic part.

--- quiz_sch/models.py
+++ quiz_sch/models.py
@@ -10,3 +10,6 @@ class Question(models.Model):
     option_3 = models.CharField(max_length=20)
     option_4 = models.CharField(max_length=20)
     right_ans = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.question
--- quiz_sch/views.py
+++ quiz_sch/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
-
+from .models import Question
+from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
+from django.contrib import messages
 # Create your views here.
 
 def home(request):
     return render(request,'quiz/base.html')
+
+def exam(request):
+    post_list = Question.objects.all()
+    paginator = Paginator(post_list, 1)
+    page_number = request.GET.get('page')
+    try:
+        post_list = paginator.page(page_number)
+    except PageNotAnInteger:
+        post_list = paginator.page(1)
+    except EmptyPage:
+        post_list = paginator.page(paginator.num_page)
+    if request.method == 'POST':
+        for post in post_list:
+            sub = request.POST.get(str(post.id))
+            if sub == post.right_ans:
+                info = messages.info(request, 'Three credits remain in your account.')
+                return render(request,'quiz/ExamPage.html', {'post_list':post_list,'ans':info})
+            elif sub != post:
+                print('incorrect')
+            else:
+                print('please select ')
+    return render(request, 'quiz/ExamPage.html', {'post_list': post_list,})
